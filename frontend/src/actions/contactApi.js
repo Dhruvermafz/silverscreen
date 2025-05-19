07c9d6f44d2ea4c04979f24f5eef2c1eb8defabd
Dhruvermafz <vermadhruv09112002@gmail.com>
2025-05-19 13:17:45 +0530

    fixed it

--- frontend/src/actions/contactApi.js
+++ frontend/src/actions/contactApi.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { API_URL } from "../../../frontend-v2/src/context/config";
+import { API_URL } from "../context/config";
 export const contactApi = createApi({
   reducerPath: "contactApi",
   baseQuery: fetchBaseQuery({ baseUrl: API_URL }), // Adjust base URL as needed
