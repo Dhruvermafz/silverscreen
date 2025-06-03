ffbb5c5e5295df155198fbd336dd269b6d2c9e83
Dhruvermafz <vermadhruv09112002@gmail.com>
2025-06-03 15:09:33 +0530

    added and fixed much of it

--- frontend/src/context/config.js
+++ frontend/src/context/config.js
@@ -1,5 +1,4 @@
-export const API_URL = "http://localhost:5000/api";
-export const BASE_URL =
+export const API_URL =
   process.env.NODE_ENV === "production"
     ? "https://silverscreen.onrender.com/api"
     : "http://localhost:5000/api";
